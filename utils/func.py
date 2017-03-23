359b120209da37a22d3784365a843903a63f469a
Winston Venderbush <winstonvenderbush@gmail.com>
2017-03-23 14:50:53 -0400

    reparsed

--- utils/func.py
+++ utils/func.py
@@ -3,15 +3,17 @@ import pprint
 
 list_of_scores = state_fragility.get_scores(test=False)
 
-#pprint.pprint(list_of_scores)
-
 cdict = {}
 
 for item in list_of_scores:
 	country = item['Country']
 	if (country not in cdict):
 		cdict[country] = {}
+	metrics = item['Metrics']
+	legit = metrics['Legitimacy']['Legitimacy Score']
+	effect = metrics['Effectiveness']['Effectiveness Score']
 	year = item['Year']
-	cdict[country][year] = item['Metrics']
+	cdict[country][year] = {'sfi':item['Metrics']['State Fragility Index'], 'legit': legit, 'effect': effect}
 
 pprint.pprint(cdict)
+
