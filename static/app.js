f9d8a305e7e34c0fd4a06a8bfae130067058a474
shamaul-d <sdilmohamed@stuy.edu>
2017-03-30 14:49:03 -0400

    work

---
+++ static/app.js
@@ -0,0 +1,8 @@
+var bar = document.getElementById('scroll');
+var dis = document.getElementById('display');
+
+var ball = function(x,y,r,p) {
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    
+}
+
