09e653b06783b95971576478819800edda15a8a3
Winston Venderbush <winstonvenderbush@gmail.com>
2017-03-31 14:50:09 -0400

    more backend updages

--- utils/func.py
+++ utils/func.py
@@ -1,4 +1,5 @@
 import state_fragility
+import math
 import pprint
 
 list_of_scores = state_fragility.get_scores(test=False)
@@ -26,16 +27,54 @@ def sfiForYear(year, clist=cdict.keys()):
 	for item in cdict:
 		if item in clist:
 			years = cdict[item]
-			pprint.pprint(years)
-			print years[1995]
-			#try:
-				#retdict[item] = cdict[item]#[year]#['sfi']
-			#except:
-				#retdict[item] = -1
-	#return retdict
+			try:
+				retdict[item] = cdict[item][year]['sfi']
+			except:
+				retdict[item] = -1
+	return retdict
+
+def legitForYear(year, clist=cdict.keys()):
+	retdict = {}
+	for item in cdict:
+		if item in clist:
+			years = cdict[item]
+			try:
+				retdict[item] = cdict[item][year]['legit']
+			except:
+				retdict[item] = -1
+	return retdict
+
+def effectForYear(year, clist=cdict.keys()):
+	retdict = {}
+	for item in cdict:
+		if item in clist:
+			years = cdict[item]
+			try:
+				retdict[item] = cdict[item][year]['effect']
+			except:
+				retdict[item] = -1
+	return retdict
+
+def findRadius(index, type):
+	radius = 0;
+	if (index < 0 or index > 120):
+		radius = -1
+		
+	elif type = "sfi":
+		area = math.abs(121 - index)
+		radius = math.sqrt(area / math.pi)
+
+	else:
+		area = math.abs(61 - index)
+		radius = math.sqrt(area / math.pi)
+
+	return radius
+
+def radiusForYear(year, country):
+	
 
 
 
-pprint.pprint(sfiForYear(2012, ['Canada']))
+pprint.pprint(sfiForYear(1999))
 
 #print countryNames()
