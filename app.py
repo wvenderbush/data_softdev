8d6609b83e5c351de1fe466fd547b7598b24af42
Winston Venderbush <winstonvenderbush@gmail.com>
2017-04-03 14:39:39 -0400

    added new flask route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-import data.func
 from flask import Flask, render_template, request, url_for
+import utils
 
 
 app = Flask(__name__)
@@ -10,7 +10,12 @@ app = Flask(__name__)
 @app.route("/home")
 def root():
 	form = request.form
-	return render_template('main.html', title = "State Fragility Indices", )
+	return render_template('main.html', title = "State Fragility Indices" )
+
+@app.route("/about")
+def about():
+	form = request.form
+	return render_template('about.html', title = "About" )
 
 
 if __name__ == "__main__":
