c9da03b9265b30f0707a71876765bf2f7610ac20
shamaul-d <sdilmohamed@stuy.edu>
2017-04-03 14:46:58 -0400

    maxery

--- static/app.js
+++ static/app.js
@@ -1,13 +1,38 @@
 var bar = document.getElementById('scroll');
 var dis = document.getElementById('display');
+var ns = 'http://www.w3.org/2000/svg';
+var countries = [];
 
-var circleMask = function(x,y,r) {
-    var mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", x);
-    c.setAttribute("cy", y);
-    c.setAttribute("r", r);
-    mask.appendChild(c);
-    return mask;
+
+var collides = function() {
+    r = parseInt(this.getAttribute('width')) / 2;
+    x = parseInt(this.getAttribute('x')) + r;
+    y = parseInt(this.getAttribute('y')) + r;
+    for (ball in countries) {
+	ro = parseInt(ball.getAttribute('width')) / 2;
+	xo = parseInt(ball.getAttribute('x')) + ro;
+	yo = parseInt(ball.getAttribute('y')) + ro;
+	if(Math.pow(xo - x, 2) + Math.pow(yo - y, 2) <= Math.pow(ro - r, 2))
+	    return true;
+    }
+    return false;
+}
+
+var country = function(c, y) {
+    var im = document.createElementNS(ns, 'image');
+    var r = -1;
+    $.get('/data',{country:c, year:y}, function(data) {
+	im.setAttribute('xlink:href', data.url);
+	im.setAttribute('width', 2 * data.radius);
+	im.setAttribute('height', 2 * data.radius);
+	r = data.radius;
+	maxx = 1400 - r;
+	maxy = 615 - r;
+	x = Math.floor(Math.random() * (maxx - r)) + r
+	y = Math.floor(Math.random() * (maxy - r)) + r
+    });
+    
+
+
 }
 
